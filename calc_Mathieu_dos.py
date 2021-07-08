aac1a39e299715557f9e6b83b45f5192fbaa681b
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2021-07-07 23:07:47 -0700

    Update calc_Mathieu_dos.py
    Added method to import eigenvalues from json format

--- calc_Mathieu_dos.py
+++ calc_Mathieu_dos.py
@@ -2,6 +2,7 @@ from math import pi
 from numpy import linspace,zeros,array,sqrt,exp
 from scipy.special import mathieu_a
 import matplotlib.pyplot as plt
+from json import load
 
 class calculate_Mathieu_dos:
     def __init__(self,nstates,k,xpoints,ypoints,xrange,yrange,**args):
@@ -43,6 +44,23 @@ class calculate_Mathieu_dos:
         else:
             self.calculate_dos[self.k]
         self.plot_dos()
+        
+    def read_json_eigenenergies(self,filepath):
+        with open(filepath) as file:
+            data=load(file)
+            data=array([[float(i[j]) for j in range(1,len(i))] for i in data[1:]])
+        for j in range(self.ypoints):
+            for a in data[j]:
+                a-=-self.xrange
+                a=round(a/(2*self.xrange/self.xpoints))
+                if a>0 and a<self.xpoints:
+                    self.eigenval[a][j]+=1.0
+        for j in range(self.ypoints):
+            smeared_dos=zeros(self.xpoints)
+            for i in range(self.xpoints):
+                gauss=array([(self.eigenval[i][j]/self.sigma/sqrt(2*pi))*exp((((i-k)*2*self.xrange/self.xpoints)/self.sigma)**2/-2) for k in range(self.xpoints)])
+                smeared_dos+=gauss
+            self.dos[:,j]+=smeared_dos
         
     def calculate_dos(self,k):
         for n in range(1,self.nstates):
