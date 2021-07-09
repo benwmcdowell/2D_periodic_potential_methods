db2c562f0fea7b0ab6d8c66cb9fdcc026dff1d80
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2021-07-09 09:42:29 -0700

    Update calc_Mathieu_dos.py
    Fixed energy scaling error

--- calc_Mathieu_dos.py
+++ calc_Mathieu_dos.py
@@ -8,18 +8,20 @@ class calculate_Mathieu_dos:
     def __init__(self,nstates,k,xpoints,ypoints,xrange,yrange,**args):
         if type(xrange)==list:
             self.energies=linspace(-min(xrange),max(xrange),xpoints)
+            self.xrange=max(xrange)-min(xrange)
         else:
             self.energies=linspace(-xrange,xrange,xpoints) #eV
+            self.xrange=xrange
         if type(yrange)==list:
             self.A=linspace(-min(yrange),max(yrange),ypoints)
+            self.yrange=max(yrange)-min(yrange)
         else:
             self.A=linspace(-yrange,yrange,ypoints) #eV
+            self.yrange=yrange
         self.eigenval=zeros((xpoints,ypoints))
         self.dos=zeros((xpoints,ypoints))
         self.x=zeros((xpoints,ypoints))
         self.y=zeros((xpoints,ypoints))
-        self.xrange=xrange
-        self.yrange=yrange
         self.nstates=nstates
         self.k=k
         self.xpoints=xpoints
@@ -53,17 +55,17 @@ class calculate_Mathieu_dos:
         for j in range(self.ypoints):
             for a in data[j]:
                 a*=pi**2*self.k**2*self.h**2/self.m/self.b/2
-                a-=-self.xrange
-                a=round(a/(2*self.xrange/self.xpoints))
+                a-=-self.energies[0]
+                a=round(a/(self.xrange/self.xpoints))
                 if a>0 and a<self.xpoints:
                     self.eigenval[a][j]+=1.0
         for j in range(self.ypoints):
             smeared_dos=zeros(self.xpoints)
             for i in range(self.xpoints):
                 if normalize:
-                    gauss=array([(self.eigenval[i][j]/self.sigma/sqrt(2*pi))*exp((((i-k)*2*self.xrange/self.xpoints)/self.sigma)**2/-2) for k in range(self.xpoints)])  #normalized gaussian
+                    gauss=array([(self.eigenval[i][j]/self.sigma/sqrt(2*pi))*exp((((i-k)*self.xrange/self.xpoints)/self.sigma)**2/-2) for k in range(self.xpoints)])  #normalized gaussian
                 if not normalize:
-                    gauss=array([self.eigenval[i][j]*exp((((i-k)*2*self.xrange/self.xpoints)/self.sigma)**2/-2) for k in range(self.xpoints)]) #unnormalized gaussian
+                    gauss=array([self.eigenval[i][j]*exp((((i-k)*self.xrange/self.xpoints)/self.sigma)**2/-2) for k in range(self.xpoints)]) #unnormalized gaussian
                 smeared_dos+=gauss
             self.dos[:,j]+=smeared_dos
         
@@ -74,8 +76,8 @@ class calculate_Mathieu_dos:
                 if n%2==0:
                     a=mathieu_a(n,q)
                     a*=pi**2*k**2*self.h**2/self.m/self.b/2
-                    a-=-self.xrange
-                    a=round(a/(2*self.xrange/self.xpoints))
+                    a-=-self.energies[0]
+                    a=round(a/(self.xrange/self.xpoints))
                     if a>0 and a<self.xpoints:
                         self.eigenval[a][j]+=1.0
         for j in range(self.ypoints):
