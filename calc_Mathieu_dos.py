f855fdf2f3c6abd258484bdb74fff531ee781263
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2021-07-13 16:47:25 -0700

    Update calc_Mathieu_dos.py
    Added methods for plotting real space projections of Mathieu functions

--- calc_Mathieu_dos.py
+++ calc_Mathieu_dos.py
@@ -1,5 +1,6 @@
 from math import pi
 from numpy import linspace,zeros,array,sqrt,exp
+from numpy.linalg import norm
 from scipy.special import mathieu_a
 import matplotlib.pyplot as plt
 from json import load
@@ -20,6 +21,8 @@ class calculate_Mathieu_dos:
             self.yrange=yrange
         self.eigenval=zeros((xpoints,ypoints))
         self.dos=zeros((xpoints,ypoints))
+        self.psi=zeros((xpoints,ypoints))
+        self.psi_smeared=zeros((xpoints,ypoints))
         self.x=zeros((xpoints,ypoints))
         self.y=zeros((xpoints,ypoints))
         self.k=k
@@ -67,6 +70,37 @@ class calculate_Mathieu_dos:
                     gauss=array([self.eigenval[i][j]*exp((((i-k)*self.xrange/self.xpoints)/self.sigma)**2/-2) for k in range(self.xpoints)]) #unnormalized gaussian
                 smeared_dos+=gauss
             self.dos[:,j]+=smeared_dos
+        self.x.T
+        self.y.T
+        self.eigenval.T
+        self.dos.T
+        self.data_type='energy'
+        
+    def read_json_eigenfunctions(self,filepath,**args):
+        if 'normalize_dos' in args:
+            normalize=True
+        else:
+            normalize=False
+        with open(filepath) as file:
+            data=load(file)
+        for i in range(1,len(data)):
+            for j in range(1,len(data)):
+                if type(data[i][j])==list:
+                    self.psi[i-1][j-1]=0.0
+                else:
+                    self.psi[i-1][j-1]=float(data[i][j])**2
+        for i in range(self.ypoints):
+            self.psi[i]/=norm(self.psi[i])
+        for i in range(self.xpoints):
+            smeared_dos=zeros(self.ypoints)
+            for j in range(self.ypoints):
+                if normalize:
+                    gauss=array([(self.psi[i][j]/self.sigma/sqrt(2*pi))*exp((((j-k)*self.yrange/self.ypoints)/self.sigma)**2/-2) for k in range(self.ypoints)])  #normalized gaussian
+                if not normalize:
+                    gauss=array([self.psi[i][j]*exp((((j-k)*self.yrange/self.ypoints)/self.sigma)**2/-2) for k in range(self.ypoints)]) #unnormalized gaussian
+                smeared_dos+=gauss
+            self.psi_smeared[:,i]+=smeared_dos
+        self.data_type='function'
         
     def calculate_dos(self,k,nstates):
         self.nstates=nstates
@@ -88,20 +122,40 @@ class calculate_Mathieu_dos:
             self.dos[:,j]+=smeared_dos
     
     def plot_dos(self):
-        plt.figure()
-        plt.title('Mathieu density of states | $\sigma$ = {}'.format(self.sigma))
-        plt.pcolormesh(self.x,self.y,self.dos,cmap='jet',shading='nearest')
-        plt.xlabel('energy / eV')
-        plt.ylabel('barrier height / eV')
-        cbar=plt.colorbar()
-        cbar.set_label('density of states / states $eV^{-1}$')
-        plt.show()
-        
-        plt.figure()
-        plt.title('Mathieu eigenenergies')
-        plt.pcolormesh(self.x,self.y,self.eigenval,cmap='jet',shading='nearest')
-        plt.xlabel('energy / eV')
-        plt.ylabel('barrier height / eV')
-        cbar=plt.colorbar()
-        cbar.set_label('number of states')
-        plt.show()
+        if self.data_type=='energy':
+            plt.figure()
+            plt.title('Mathieu density of states | $\sigma$ = {}'.format(self.sigma))
+            plt.pcolormesh(self.x,self.y,self.dos,cmap='jet',shading='nearest')
+            plt.ylabel('energy / eV')
+            plt.xlabel('barrier height / eV')
+            cbar=plt.colorbar()
+            cbar.set_label('density of states / states $eV^{-1}$')
+            plt.show()
+            
+            plt.figure()
+            plt.title('Mathieu eigenenergies')
+            plt.pcolormesh(self.x,self.y,self.eigenval,cmap='jet',shading='nearest')
+            plt.ylabel('energy / eV')
+            plt.xlabel('barrier height / eV')
+            cbar=plt.colorbar()
+            cbar.set_label('number of states')
+            plt.show()
+            
+        if self.data_type=='function':            
+            plt.figure()
+            plt.title('Mathieu density of states | $\sigma$ = {}'.format(self.sigma))
+            plt.pcolormesh(self.x,self.y,self.psi_smeared,cmap='jet',shading='nearest')
+            plt.ylabel('energy / eV')
+            plt.xlabel('position / $\AA^{-1}$')
+            cbar=plt.colorbar()
+            cbar.set_label('density of states / states $eV^{-1}$')
+            plt.show()
+            
+            plt.figure()
+            plt.title('Mathieu functions | $\sigma$ = 0.0')
+            plt.pcolormesh(self.x,self.y,self.psi,cmap='jet',shading='nearest')
+            plt.ylabel('energy / eV')
+            plt.xlabel('position / $\AA^{-1}$')
+            cbar=plt.colorbar()
+            cbar.set_label('density of states / states $eV^{-1}$')
+            plt.show()
