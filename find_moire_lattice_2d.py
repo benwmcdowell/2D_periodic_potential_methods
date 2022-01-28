88b54ec72364c3f69b1e48873c8ed991657434a1
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2022-01-28 09:46:04 -0800

    Update find_moire_lattice_2d.py
    Simplified procedure to calculate the difference only between substrate and adsorbate lattices

--- find_moire_lattice_2d.py
+++ find_moire_lattice_2d.py
@@ -17,10 +17,10 @@ class moire():
     def read_file(self,ifile):
         with open(ifile,'r') as f:
             self.mindiff=json.load(f)
-        self.apts=np.shape(self.mindiff)[1]
+        self.apts=np.shape(self.mindiff)[0]
         self.angle=np.array([i*np.pi*2/(self.apts-1) for i in range(self.apts)])
             
-    def calculate(self,nprocs,apts,lpts,slv,alv,elv):
+    def calculate(self,nprocs,apts,lpts,slv,alv,elv,**args):
         if 'max_length' in args:
             self.max_length=args['max_length']
         else:
@@ -33,36 +33,48 @@ class moire():
         self.nprocs=nprocs
         self.angle=np.array([i*np.pi*2/(apts-1) for i in range(apts)])
         pool=ProcessPool(self.nprocs)
-        output=pool.map(self.single_process, [i for i in range(self.apts) for j in range(np.shape(elv)[0])], [j for i in range(self.apts) for j in range(np.shape(elv)[0])])
+        output=pool.map(self.single_process, [i for i in range(self.apts)])
         self.mindiff=sum(output)
         pool.close()
         
-    def single_process(self,a,e):
+    def single_process(self,a):
         import numpy as np
         lrange=[int(i) for i in range(-self.lpts,self.lpts+1)]
         lrange.remove(0)
-        tempdiff=np.zeros((np.shape(self.elv)[0],self.apts))
-        tempdiff[e,a]+=np.inf
+        tempdiff=np.zeros((2,self.apts))
+        for i in range(2):
+            tempdiff[i,a]=np.inf
         rot=np.array([[np.cos(self.angle[a]),np.sin(self.angle[a])],[-np.sin(self.angle[a]),np.cos(self.angle[a])]])
         for i in lrange:
             for j in lrange:
-                for k in lrange:
-                    for l in lrange:
-                        pos=self.slv[0]*i+self.slv[1]*j+np.dot(self.alv[0]*k,rot)+np.dot(self.alv[1]*l,rot)
-                        if pos<self.max_length:
-                            pos=pos%self.elv[e]
-                            if np.linalg.norm(pos)<tempdiff[e,a]:
-                                tempdiff[e,a]=np.linalg.norm(pos)
+                pos=self.slv[0]*i+self.slv[1]*j
+                tempalv=np.array([np.dot(k,rot) for k in self.alv])
+                pos=np.dot(pos,np.linalg.inv(tempalv))
+                if np.linalg.norm(pos)<self.max_length:
+                    for k in range(2):
+                        while pos[k]>1.0 or pos[k]<0.0:
+                            if pos[k]>1.0:
+                                pos[k]-=1.0
+                            if pos[k]<0.0:
+                                pos[k]+=1.0
+                    pos=np.dot(pos,tempalv)
+                    if np.linalg.norm(pos)<tempdiff[0,a]:
+                        tempdiff[0,a]=np.linalg.norm(pos)
+                        pos=self.slv[0]*i+self.slv[1]*j
+                        for k in lrange:
+                            for l in lrange:
+                                for m in lrange:
+                                    epos=elv[0]*k+elv[1]*l+elv[2]*m
+                                    if np.linalg.norm(pos-epos)<tempdiff[1,a]:
+                                        tempdiff[1,a]=np.linalg.norm(pos-epos)
         return tempdiff
     
     def plot_moire(self):
         self.angle*=180/np.pi
-        self.fig,self.axs=plt.subplots(np.shape(self.mindiff)[0]+1,1,sharex=True)
-        for i in range(np.shape(self.mindiff)[0]):
-            self.axs[i].plot(self.angle,self.mindiff[i])
-            self.axs[i].set(ylabel='minimum difference / $\AA$')
-        self.axs[-1].plot(self.angle,sum(np.array([self.mindiff[i] for i in range(np.shape(self.mindiff)[0])])))
-        self.axs[-1].set(xlabel='misorientation angle / degrees')
+        self.fig,self.ax=plt.subplots(1,1)
+        self.ax.plot(self.angle,self.mindiff)
+        self.ax.set(ylabel='minimum difference / nm')
+        self.ax.set(xlabel='misorientation angle / degrees')
         self.fig.show()
     
 if __name__ == '__main__':
@@ -70,7 +82,7 @@ if __name__ == '__main__':
     nprocs=1
     output='./moire_output'
     try:
-        opts,args=getopt.getopt(sys.argv[1:],'p:s:a:n:e:o:',['processors=','substrate=','adlayer=','npts=','exp=','-output'])
+        opts,args=getopt.getopt(sys.argv[1:],'p:s:a:n:e:o:m:',['processors=','substrate=','adlayer=','npts=','exp=','output=','max_length='])
     except getopt.GetoptError:
         print('error in command line syntax')
         sys.exit(2)
@@ -89,9 +101,13 @@ if __name__ == '__main__':
             lpts=int(lpts)
         if i in ['-o','--output']:
             output=j
+        if i in ['-m','--max_length']:
+            max_length=float(j)
+        else:
+            max_length=np.inf
     try:
         main=moire()
-        main.calculate(nprocs,apts,lpts,slv,alv,elv)
+        main.calculate(nprocs,apts,lpts,slv,alv,elv,max_length=max_length)
         main.write_file(output)
     except NameError:
         print('Error: lattice vectors not defined')
