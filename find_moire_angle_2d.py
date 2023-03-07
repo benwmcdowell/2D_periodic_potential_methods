21414224ebc6a13721e2e86b4a10cf6a8ba22798
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2023-03-07 15:41:45 -0800

    commit to backup savefile

--- find_moire_angle_2d.py
+++ find_moire_angle_2d.py
@@ -3,16 +3,127 @@ import numpy as np
 
 #elv is the experimental lattice vectors to be matched
 #lv1 and lv2 are the theoretical lattice vectors defining the moire pattern, which form an incident angle of theta
-def plot_moire_angle(elv,lv1,name1,lv2,name2,**args):
-    npts=361
+def plot_moire_angle_real(elv,lv1,name1,lv2,name2,**args):
+    if 'npts' in args:
+        npts=args['npts']
+    else:
+        npts=361
     theta=np.array([i/(npts-1)*360 for i in range(npts)])
     theta*=np.pi/180
     
-    tempvar=np.zeros((3,3))
-    tempvar[:2,:2]+=elv
-    tempvar[2,2]=1.0
-    elv[0]=(2*np.pi*np.cross(tempvar[1],tempvar[2])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
-    elv[1]=(2*np.pi*np.cross(tempvar[2],tempvar[0])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
+    if 'errors' in args:
+        errors=args['errors']
+    else:
+        errors=None
+    
+    if 'reciprocal' in args:
+        for i in range(2):
+            elv[i]/=(np.linalg.norm(elv[i]))**2
+            elv[i]*=np.pi*2
+        
+            tempvar=np.zeros((3,3))
+            tempvar[:2,:2]+=lv1
+            tempvar[2,2]=1.0
+            lv1[0]=(2*np.pi*np.cross(tempvar[1],tempvar[2])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
+            lv1[1]=(2*np.pi*np.cross(tempvar[2],tempvar[0])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
+            
+            tempvar=np.zeros((3,3))
+            tempvar[:2,:2]+=lv2
+            tempvar[2,2]=1.0
+            lv2[0]=(2*np.pi*np.cross(tempvar[1],tempvar[2])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
+            lv2[1]=(2*np.pi*np.cross(tempvar[2],tempvar[0])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
+            
+    if errors:
+        for i in range(2):
+            errors[i]*=np.linalg.norm(elv[i])
+            
+    if 'rotate_lv2' in args:
+        theta2=30/180*np.pi
+        rot=np.array([[np.cos(theta2),np.sin(theta2)],[-np.sin(theta2),np.cos(theta2)]])
+        for i in range(2):
+            lv2[i]=np.dot(lv2[i],rot)
+            tempvar=np.linalg.norm(lv2[i])
+            lv2[i]/=tempvar
+            lv2[i]*=np.cos(theta2)*tempvar/2
+    
+    diff=np.zeros((2,npts))
+    shifts=np.zeros((2,npts,4))
+    for i in range(2):
+        for j in range(npts):
+            rot=np.array([[np.cos(theta[j]),np.sin(theta[j])],[-np.sin(theta[j]),np.cos(theta[j])]])
+            pos=np.dot(elv[i],np.linalg.inv(lv1))
+            for k in range(2):
+                while pos[k]>1.0 or pos[k]<0.0:
+                    if pos[k]>1.0:
+                        pos[k]-=1.0
+                        shifts[i,j,k]+=1
+                    elif pos[k]<0.0:
+                        pos[k]+=1.0
+                        shifts[i,j,k]-=1
+            pos=np.dot(pos,lv1)
+            
+            pos=np.dot(pos,np.linalg.inv(np.dot(lv2,rot)))
+            for k in range(2):
+                while pos[k]>1.0 or pos[k]<0.0:
+                    if pos[k]>1.0:
+                        pos[k]-=1.0
+                        shifts[i,j,k+2]+=1
+                    if pos[k]<0.0:
+                        pos[k]+=1.0
+                        shifts[i,j,k+2]-=1
+            pos=np.dot(pos,np.dot(lv2,rot))
+            
+            diff[i,j]+=np.linalg.norm(pos)
+            
+    theta*=180/np.pi
+                        
+    fig,axs=plt.subplots(3,1)
+    for i in range(2):
+            axs[i].plot(theta,diff[i])
+            if errors:
+                axs[i].plot(theta,diff[i]-errors[i],label='min')
+                axs[i].plot(theta,diff[i]+errors[i],label='max')
+    axs[2].plot(theta,diff[0]+diff[1])
+    
+    for i in range(3):
+        axs[i].plot(theta,np.zeros(npts),linestyle='dashed',color='black')
+    
+    if errors:
+        axs[2].plot(theta,diff[0]+diff[1]-np.sqrt(errors[0]**2+errors[1]**2),label='min')
+        axs[2].plot(theta,diff[0]+diff[1]+np.sqrt(errors[0]**2+errors[1]**2),label='max')
+            
+    axs[-1].set(xlabel='theta / degrees')
+    axs[0].set(ylabel='$V_{e1}$')
+    axs[1].set(ylabel='$V_{e2}$')
+    axs[2].set(ylabel='$V_{e1} + V_{e2}$')
+    if errors:
+        handles,labels=axs[0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc='upper right')
+    fig.show()
+    
+    print('errors:')
+    print(errors)
+    
+    return shifts,theta
+    
+    
+def plot_moire_angle_rec(elv,lv1,name1,lv2,name2,**args):
+    if 'errors' in args:
+        errors=args['errors']
+    else:
+        errors=None
+        
+    npts=3601
+    theta=np.array([i/(npts-1)*360 for i in range(npts)])
+    theta*=np.pi/180
+    
+    for i in range(2):
+        elv[i]/=(np.linalg.norm(elv[i]))**2
+        elv[i]*=np.pi*2
+        
+    if errors:
+        for i in range(2):
+            errors[i]*=np.linalg.norm(elv[i])
     
     tempvar=np.zeros((3,3))
     tempvar[:2,:2]+=lv1
@@ -26,48 +137,73 @@ def plot_moire_angle(elv,lv1,name1,lv2,name2,**args):
     lv2[0]=(2*np.pi*np.cross(tempvar[1],tempvar[2])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
     lv2[1]=(2*np.pi*np.cross(tempvar[2],tempvar[0])/np.dot(tempvar[0],np.cross(tempvar[1],tempvar[2])))[:2]
     
-    #heirarchy of diff is (experimental lattice vector, theoretical lattice vector, +/-)
-    diff=np.zeros((2,2,2,2,npts))
+    if 'rotate_lv2' in args:
+        theta2=30/180*np.pi
+        rot=np.array([[np.cos(theta2),np.sin(theta2)],[-np.sin(theta2),np.cos(theta2)]])
+        for i in range(2):
+            lv2[i]=np.dot(lv2[i],rot)
+            tempvar=np.linalg.norm(lv2[i])
+            lv2[i]/=tempvar
+            lv2[i]*=np.cos(theta2)*tempvar/2
+            
+    if 'reduce_elv' in args:
+        for i in range(2):
+            elv[i]=np.dot(elv[i],np.linalg.inv(lv1))
+            for j in range(2):
+                while elv[i][j]>1.0 or elv[i][j]<0.0:
+                    if elv[i][j]>1.0:
+                        elv[i][j]-=1.0
+                    elif elv[i][j]<0.0:
+                        elv[i][j]+=1.0
+            elv[i]=np.dot(elv[i],lv1)
+        
+    print(elv)
+    print(lv1)
+    print(lv2)
+    
+    diff=np.zeros((2,4,npts))
     for i in range(2):
         for j in range(2):
             for k in range(2):
-                for l in range(2):
-                    for m in range(npts):
-                        rot=np.array([[np.cos(theta[m]),-np.sin(theta[m])],[np.sin(theta[m]),np.cos(theta[m])]])
-                        if l==0:
-                            sf=1
-                        if l==1:
-                            sf=-1
-                        diff[i,j,k,l,m]+=np.linalg.norm(lv1[j]+sf*np.dot(lv2[k],rot)-elv[i])
+                for l in range(npts):
+                    rot=np.array([[np.cos(theta[l]),np.sin(theta[l])],[-np.sin(theta[l]),np.cos(theta[l])]])
+                    
+                    pos=[elv[i]*(-2*j+1)+(-2*k+1)*lv1[m] for m in range(2)]                 
+                        
+                    diff[i,2*j+k,l]=min(np.linalg.norm(m+np.dot(lv2[0],rot)-np.dot(lv2[1],rot)) for m in pos)
                         
     theta*=180/np.pi
     
-    fig,axs=plt.subplots(4,2,sharex=True)
-    for j in range(2):
-        for x in range(4):
-            if x==0:
-                k=0
-                l=0
-            if x==1:
-                k=0
-                l=1
-            if x==2:
-                k=1
-                l=0
-            if x==3:
-                k=1
-                l=1
+    minval=np.zeros((4,2))
+    minangle=np.zeros((4,2))
+    for i in range(2):
+        for j in range(4):
+            minval[j,i]=np.min(diff[i,j])
+            minangle[j,i]=theta[np.argmin(diff[i,j])]
             
-            axs[x,j].plot(theta,diff[0,j,k,l],label='$V_{e1}$')
-            axs[x,j].plot(theta,diff[1,j,k,l],label='$V_{e2}$')
-        axs[-1,j].set(xlabel='theta / degrees')
+    print('minimum differences:')
+    print(minval)
+    print('angles of minimum differences:')
+    print(minangle)
+    print('errors:')
+    print(errors)
+    
+    fig,axs=plt.subplots(4,2,sharex=True)
+    for i in range(2):
+        for j in range(4):
+            axs[j,i].plot(theta,diff[i,j])
+            if errors:
+                axs[j,i].plot(theta,diff[i,j]+errors[i],label='max')
+                axs[j,i].plot(theta,diff[i,j]-errors[i],label='min')
+        axs[-1,i].set(xlabel='theta / degrees')
         
-    axs[0,0].set(title='V1 {}'.format(name1))
-    axs[0,1].set(title='V2 {}'.format(name1))
-    axs[0,0].set(ylabel='+V1 {}'.format(name2))
-    axs[1,0].set(ylabel='-V1 {}'.format(name2))
-    axs[2,0].set(ylabel='+V2 {}'.format(name2))
-    axs[3,0].set(ylabel='-V2 {}'.format(name2))
-    handles,labels=axs[0,0].get_legend_handles_labels()
-    fig.legend(handles, labels, loc='upper right')
+    axs[0,0].set(title='Ve1')
+    axs[0,1].set(title='Ve2')
+    axs[0,0].set(ylabel='Va + Ve')
+    axs[1,0].set(ylabel='-Va + Ve')
+    axs[2,0].set(ylabel='+Va - Ve')
+    axs[3,0].set(ylabel='-Va - Ve')
+    if errors:
+        handles,labels=axs[0,0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc='upper right')
     fig.show()
--- find_moire_lattice_2d.py
+++ find_moire_lattice_2d.py
@@ -11,69 +11,85 @@ class moire():
         pass
     
     def write_file(self,ofile):
-        with open(ofile,'w+') as f:
-            f.write(json.dumps([list(i) for i in self.mindiff]))
+        with open(ofile+'_differences','w+') as f:
+            f.write(json.dumps([list(i) for i in [self.mindiff]]))
+        with open(ofile+'_coefficients','w+') as f:
+            f.write(json.dumps([list(self.coeff[:,i]) for i in range(4)]))
             
     def read_file(self,ifile):
-        with open(ifile,'r') as f:
-            self.mindiff=json.load(f)
-        self.apts=np.shape(self.mindiff)[1]
+        with open(ifile+'_diff','r') as f:
+            self.mindiff=json.load(f)[0]
+        with open(ifile+'_coefficients','r') as f:
+            self.coeff=json.load(f)
+        self.apts=len(self.mindiff)
         self.angle=np.array([i*np.pi*2/(self.apts-1) for i in range(self.apts)])
             
-    def calculate(self,nprocs,apts,lpts,slv,alv,elv,**args):
+    def calculate(self,nprocs,apts,slv,alv,**args):
         if 'max_length' in args:
             self.max_length=args['max_length']
         else:
-            self.max_length=np.inf
+            self.max_length=100
+        print('max vector length is: {}'.format(self.max_length))
         self.alv=alv
         self.slv=slv
-        self.elv=elv
         self.apts=apts
-        self.lpts=lpts
         self.nprocs=nprocs
         self.angle=np.array([i*np.pi*2/(apts-1) for i in range(apts)])
-        pool=ProcessPool(self.nprocs)
-        output=pool.map(self.single_process, [i for i in range(self.apts)])
-        self.mindiff=sum(output)
-        pool.close()
+        self.mindiff=np.zeros((2,self.apts))
+        self.coeff=np.zeros((2,self.apts,4))
+        if self.nprocs>1:
+            pool=ProcessPool(self.nprocs)
+            output=pool.map(self.single_process, [i for i in range(self.apts)])
+            for i in range(self.apts):
+                self.mindiff+=output[i][0]
+                self.coeff+=output[i][1]
+            pool.close()
+        else:
+            for i in range(self.apts):
+                tempvar1,tempvar2=self.single_process(i)
+                for j in range(2):
+                    self.mindiff[j,i]=tempvar1[j,i]
+                    self.coeff[j,i]+=tempvar2[j,i]
         
     def single_process(self,a):
         import numpy as np
-        lrange=[int(i) for i in range(-self.lpts,self.lpts+1)]
-        lrange.remove(0)
-        tempdiff=np.zeros((2,self.apts))
+        lpts=np.array([int(np.round(self.max_length/np.linalg.norm(self.alv[i]))) for i in range(2)])
+        lrange=[[j for j in range(-lpts[i],lpts[i]+1)] for i in range(2)]
         for i in range(2):
-            tempdiff[i,a]=np.inf
+            lrange[i].remove(0)
+        tempdiff=np.zeros((2,self.apts))
+        coeff=np.zeros((2,self.apts,4))
         rot=np.array([[np.cos(self.angle[a]),np.sin(self.angle[a])],[-np.sin(self.angle[a]),np.cos(self.angle[a])]])
-        for i in lrange:
-            for j in lrange:
-                pos=self.slv[0]*i+self.slv[1]*j
-                tempalv=np.array([np.dot(k,rot) for k in self.alv])
-                pos=np.dot(pos,np.linalg.inv(tempalv))
-                if np.linalg.norm(pos)<self.max_length:
-                    for k in range(2):
-                        while pos[k]>1.0 or pos[k]<0.0:
-                            if pos[k]>1.0:
-                                pos[k]-=1.0
-                            if pos[k]<0.0:
-                                pos[k]+=1.0
-                    pos=np.dot(pos,tempalv)
-                    if np.linalg.norm(pos)<tempdiff[0,a]:
-                        tempdiff[0,a]=np.linalg.norm(pos)
-                        pos=self.slv[0]*i+self.slv[1]*j
-                        for k in lrange:
-                            for l in lrange:
-                                for m in lrange:
-                                    epos=elv[0]*k+elv[1]*l+elv[2]*m
-                                    if np.linalg.norm(pos-epos)<tempdiff[1,a]:
-                                        tempdiff[1,a]=np.linalg.norm(pos-epos)
-        return tempdiff
+        for k in range(2):
+            tempdiff[k,a]=np.inf
+            for i in lrange[0]:
+                for j in lrange[1]:
+                    temp_coeff=np.array([0,0,i,j])
+                    pos=self.alv[0]*i+self.alv[1]*j
+                    pos=np.dot(pos,rot)
+                    pos=np.dot(pos,np.linalg.inv(self.slv))
+                    while pos[k]>1.0 or pos[k]<0.0:
+                        if pos[k]>1.0:
+                            pos[k]-=1.0
+                            temp_coeff[k]+=1
+                        if pos[k]<0.0:
+                            pos[k]+=1.0
+                            temp_coeff[k]-=1
+                    pos=np.dot(pos,self.slv)
+                    if np.linalg.norm(pos)<tempdiff[k,a]:
+                        tempdiff[k,a]=np.linalg.norm(pos)
+                        coeff[k,a]=temp_coeff
+                    
+        return tempdiff,coeff
     
     def plot_moire(self):
         self.angle*=180/np.pi
-        self.fig,self.axs=plt.subplots(2,1,sharex=True)
-        for i in range(2):
-            self.axs[i].plot(self.angle,self.mindiff[i])
+        self.fig,self.axs=plt.subplots(3,1,sharex=True)
+        for i in range(3):
+            if i<2:
+                self.axs[i].plot(self.angle,self.mindiff[i])
+            else:
+                self.axs[i].plot(self.angle,self.mindiff[0]+self.mindiff[1])
             self.axs[i].set(ylabel='lattice mismatch / nm')
         self.axs[-1].set(xlabel='misorientation angle / degrees')
         self.fig.show()
@@ -83,7 +99,7 @@ if __name__ == '__main__':
     nprocs=1
     output='./moire_output'
     try:
-        opts,args=getopt.getopt(sys.argv[1:],'p:s:a:n:e:o:m:',['processors=','substrate=','adlayer=','npts=','exp=','output=','max_length='])
+        opts,args=getopt.getopt(sys.argv[1:],'p:s:a:n:o:m:',['processors=','substrate=','adlayer=','npts=','output=','max_length='])
     except getopt.GetoptError:
         print('error in command line syntax')
         sys.exit(2)
@@ -94,12 +110,8 @@ if __name__ == '__main__':
             slv=np.array([float(k) for k in j.split(',')]).reshape((2,2))
         if i in ['-a','--adlayer']:
             alv=np.array([float(k) for k in j.split(',')]).reshape((2,2))
-        if i in ['-e','--exp']:
-            elv=np.array([float(k) for k in j.split(',')]).reshape((len(j.split(','))//2,2))
         if i in ['-n','--npts']:
-            apts,lpts=j.split(',')
-            apts=int(apts)
-            lpts=int(lpts)
+            apts=int(j)
         if i in ['-o','--output']:
             output=j
         if i in ['-m','--max_length']:
@@ -108,7 +120,7 @@ if __name__ == '__main__':
             max_length=np.inf
     try:
         main=moire()
-        main.calculate(nprocs,apts,lpts,slv,alv,elv,max_length=max_length)
+        main.calculate(nprocs,apts,slv,alv,max_length=max_length)
         main.write_file(output)
     except NameError:
         print('Error: lattice vectors not defined')
